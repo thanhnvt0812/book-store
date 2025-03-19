0be7844c2e979a0e61c8ee2f2ba53f1ddcdb6587
thanhnvt0812 <thanhnvt.4work@gmail.com>
2025-03-19 21:53:56 +0700

    update to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,13 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     strictPort: true,
-    allowedHosts: ["book-store-production-9863.up.railway.app"], // Thêm host vào đây
+    allowedHosts: ["book-store-production-9863.up.railway.app"], // Thêm host vào danh sách cho phép
     cors: true, // Cho phép CORS nếu cần
+    hmr: {
+      protocol: "wss",
+      host: "book-store-production-9863.up.railway.app",
+      clientPort: 443, // Đảm bảo WebSocket chạy trên cổng HTTPS
+    },
   },
   build: {
     outDir: "dist",
