81a47cfb9e32a5dfb1ba2e8a2be1b6aa036c9a17
thanhnvt0812 <thanhnvt.4work@gmail.com>
2025-03-19 21:49:26 +0700

    update vite port

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
-    port: Number(import.meta.env.VITE_PORT) || 5173, // Dùng biến môi trường nếu có
+    port: 5173, // Dùng biến môi trường nếu có
   },
   build: {
     outDir: "dist",
