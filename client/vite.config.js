d1a15b5e7191036ba74391267f70f52dea257193
thanhnvt0812 <thanhnvt.4work@gmail.com>
2025-03-20 11:28:36 +0700

    update vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,19 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    allowedHosts: ["book-store-production-9863.up.railway.app"],
-    hmr: {
-      clientPort: 443, // Fix lỗi WebSocket
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "esnext", // Fix lỗi không tương thích
-    },
+  build: {
+    outDir: "dist", // Đảm bảo output directory là 'dist'
   },
 });
