60e18d33f52a305e3809844425183bb7cb1ae44e
thanhnvt0812 <thanhnvt.4work@gmail.com>
2025-03-20 18:17:13 +0700

    update vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist", // Đảm bảo output directory là 'dist'
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://book-store-ylxg.vercel.app",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
